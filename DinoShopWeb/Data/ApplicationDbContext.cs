f5b4014d1bf150c24f6e5e0708bb3624925f440b
Mastoropoulos-Loukas <loukasmastoropoulos@gmail.com>
2022-10-10 20:42:48 +0300

    db context initial commit

---
+++ DinoShopWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DinoShopWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DinoShopWeb.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; } 
+    }
+}
